030c95a697a2cbeda2692b4dfe92884a656d41c0
Ksenia Churina <k.churina@gmail.com>
2020-04-04 15:00:27 +0300

    Aded 4 laba - Stenography

---
+++ sourcecode/Steganography/Steganography.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace Steganography
+{
+    public partial class Steganography : Form
+    {
+        private Bitmap bmp = null;
+        private string extractedText = string.Empty;
+
+        public Steganography()
+        {
+            InitializeComponent();
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)imagePictureBox.Image;
+            string text = dataTextBox.Text;
+
+            if (text.Equals(""))
+            {
+                MessageBox.Show("The text you want to hide can't be empty", "Warning");
+
+                return;
+            }
+
+            bmp = SteganographyHelper.embedText(text, bmp);
+            MessageBox.Show("Your text was hidden in the image successfully!\n", "Done");
+
+
+            notesLabel.Text = "Notes: don't forget to save your new image.";
+            notesLabel.ForeColor = Color.OrangeRed;
+        }
+
+        private void extractButton_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)imagePictureBox.Image;
+
+            string extractedText = SteganographyHelper.extractText(bmp);
+            dataTextBox.Text = extractedText;
+        }
+
+        private void imageToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files (*.jpeg; *.png; *.bmp)|*.jpg; *.png; *.bmp";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                imagePictureBox.Image = Image.FromFile(open_dialog.FileName);
+            }
+        }
+
+        private void imageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (save_dialog.FilterIndex)
+                {
+                    case 0:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Png);
+                        }break;
+                    case 1:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Bmp);
+                        } break;
+                }
+
+                notesLabel.Text = "Notes:";
+                notesLabel.ForeColor = Color.Black;
+            }
+        }
+
+        private void textToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Text Files|*.txt";
+
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(save_dialog.FileName, dataTextBox.Text);
+            }
+        }
+
+        private void textToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Text Files|*.txt";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                dataTextBox.Text = File.ReadAllText(open_dialog.FileName);
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To encrypt text in an image:\n1. Use a standard image or upload your own using File-Open-Image (preferred PNG format).\n2. Enter the text in the text box or download from a text file in the format txt (use only english letters!!): File-Open-Text.\n3. Click 'Hide.'\n4. Do not forget to save the image with the text (File-Save-Image). You can also save text: File-Save-Text.\n\nTo extract text from an image:\n1. Upload a picture in which there is encrypted text using: File-Open-Image.\n2. Click 'Extract.'\n3. The decrypted text appears in the text box. You can save it: File-Save-Text.");
+        }
+    }
+}
